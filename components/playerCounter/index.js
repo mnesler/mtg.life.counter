ee43fa0aa6cd05f66705117b06d28892fcea42bf
max nesler <maxnesler@gmail.com>
2018-02-07 22:01:37 -0500

    too muich beer

--- components/playerCounter/index.js
+++ components/playerCounter/index.js
@@ -8,26 +8,29 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 const styles ={
-    button: {
-        width: '33%',
-        height: '50%',
-        backgroundColor: 'blue'
+    center: {
+        position: 'absolute',
+        margin: 'auto',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        width: 0,
+        height: 0,
+        border: '1px red dashed'
     },
-    lifeDisplay: {
-        width: 'auto',
-        height: 100,
-        textAlign: 'center',
-        fontSize: 22,
-        color: "Red",
-        background: "#F5A9A9"
-    },
-    uiContainer:{
-        height: '50%',
-        border: 1,
-        borderColor: 'Green'
-    }, 
-    rowStyle: {
-        height: '50%'
+    lifeTotal: {
+        fontSize: 130,
+        position: 'absolute',
+        margin: 'auto',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        width: 0,
+        height: 0,
+        border: '1px blue dashed'
+       
     }
 }
 export default class App extends React.Component {
@@ -35,7 +38,6 @@ export default class App extends React.Component {
       super(props);
       this.state = {
         total: 40,
-        playerData:  this.props.playerData,
       };
     }
 
@@ -53,7 +55,12 @@ export default class App extends React.Component {
         //It's not recommended to apply the bind within the render function 
         //since it will do it every time the component is rendered. you can move it to some function which runs at the start of the lifecycle
         return (
-          <div> asdfasdf</div>
+          <div style={styles.center}>
+            <div style={styles.lifeTotal}>{this.state.total}</div>
+            
+
+          </div>
+
          // <Row style={styles.rowStyle}>
          // <FlatButton style={styles.button} label="+" onTouchTap={() => this.handlePlusClick()} />
          // <div style={styles.lifeDisplay}><span style={styles.counterText}>{this.state.total}</span></div>
--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './components/counter.js'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MediaQuery from './components/mediaquerydemo.js'
-import MtgButton from './components/mtgButton.js'
+import PlayerCounter from './components/playerCounter'
 
 const styles ={
   container: {
     //minWidth: 414,
     //minHeight: 736, 
-    borderWidth: 0.5,
-    borderColor: '#d6d7da',
+    //borderWidth: 0.5,
+    //borderColor: '#d6d7da',
   }
 }
 
@@ -24,11 +22,9 @@ export default class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
-      <div style={styles.container}>
-     
-      <Counter playerData={this.state.p1} />
+      <div>
+      <PlayerCounter />
       <br />
-    {/*<Counter playerData={this.state.p2}/>*/}
       </div>
         </MuiThemeProvider>
     )}
